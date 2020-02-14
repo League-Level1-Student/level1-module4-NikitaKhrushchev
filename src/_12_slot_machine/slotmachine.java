e3e09a5c3fe8753ca0c7b6c1d4d7af95ca065f50
League Student <league@iMac-94.attlocal.net>
2020-02-13 18:25:29 -0800

    changes

--- src/_12_slot_machine/slotmachine.java
+++ src/_12_slot_machine/slotmachine.java
@@ -2,8 +2,11 @@ package _12_slot_machine;
 
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -12,45 +15,82 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class slotmachine {
+public class slotmachine implements ActionListener {
+int e = 0;
 JFrame fr = new JFrame();
-JPanel p1 = new JPanel();
-JPanel p2 = new JPanel();
-JPanel p3 = new JPanel();
-JLabel l1;
+JPanel p = new JPanel();
+JButton b = new JButton();
+JLabel l;
 JLabel l2;
 JLabel l3;
-JButton b = new JButton();
-void setup() throws MalformedURLException{
+JLabel[] ll = new JLabel[3];
+int[] numList = new int[3];
+Random r = new Random();
+void setup() {
 	fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	fr.add(p1);
-	fr.add(p2);
-	fr.add(p3);
-	fr.add(b);
-	l1 = createLabelImage("circle.jpg");
-	
-	l2 = createLabelImage("square.jpg");
-	l3 = createLabelImage("star.jpg");
-	l1.setSize(40, 40);
-	l2.setSize(40, 40);
-	l3.setSize(40, 40);
-	fr.add(l1);
-	fr.add(l2);
-	fr.add(l3);
+	b.addActionListener(this);
+	fr.add(p);
+	fr.setVisible(true);
+	newSet();
 	fr.pack();
+}
+void newSet() {
+	for (int i = 0; i < numList.length; i++) {
+		numList[i] = r.nextInt(3);
+		if(e>0) {
+		p.remove(ll[i]);
+		}
+	}
+	nextSet(numList);
+	for (int i = 0; i < ll.length; i++) {
+		p.add(ll[i]);
+		e+=1;
+	}
+	b.setText("spin");
+	p.add(b);
 	fr.setVisible(true);
 }
-private JLabel createLabelImage(String fileName) throws MalformedURLException{
-    URL imageURL = getClass().getResource(fileName);
-if (imageURL == null){
-	System.err.println("Could not find image " + fileName);
-	return new JLabel();
-}
-//Icon icon = new ImageIcon(imageURL);
-ImageIcon icon = new ImageIcon(new ImageIcon(fileName).getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
-JLabel imageLabel = new JLabel();
-imageLabel.setIcon(icon);
-return imageLabel;
+
+private JLabel[] nextSet(int[] numList){
+	for (int i = 0; i < numList.length; i++) {
+		if (numList[i] == 0) {
+			ll[i] = createLabelImage("circle.png");
+		}
+		if (numList[i] == 1) {
+			ll[i] = createLabelImage("star.png");
+		}
+		if (numList[i] == 2) {
+			ll[i] = createLabelImage("square.png");
+		}
+	}
+	System.out.println(numList.length);
+	return ll;
+	
 }
 
+
+private JLabel createLabelImage(String fileName){
+    URL imageURL = getClass().getResource(fileName);
+Icon icon = new ImageIcon(imageURL);
+JLabel imageLabel = new JLabel(icon);
+return imageLabel;
 }
+@Override
+public void actionPerformed(ActionEvent e) {
+	
+	if((numList[0]== numList[0])&&(numList[2]==numList[1])) {
+		fr.dispose();
+		JFrame c = new JFrame();
+		JLabel o = new JLabel();
+		o.setText("you win");
+		c.add(o);
+		c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c.pack();
+		c.setVisible(true);
+	}
+	else{
+		newSet();
+	}
+}
+}
+
